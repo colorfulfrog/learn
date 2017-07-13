a1c9f6d340718b8dc76c740bd41e4a99b72c1e11
colorfulfrog <671579557@qq.com>
2017-07-13 12:18:35 +0800

    添加Swagger ui支持

--- mq-sender/src/main/java/com/elead/ppm/mqsender/controller/SendController.java
+++ mq-sender/src/main/java/com/elead/ppm/mqsender/controller/SendController.java
@@ -9,15 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.elead.platform.common.api.message.SimpleTextMailDto;
 import com.elead.ppm.mqsender.sender.MailSender;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(description="消息发送服务")
 @RestController
 @RequestMapping("/msg")
 public class SendController {
 	@Autowired
 	private MailSender mailSender;
 	
-	
+	@ApiOperation("发送文本邮件")
 	@PostMapping("/mail")
-	public void sendMail(@RequestBody SimpleTextMailDto mail){
+	public void sendMail(@ApiParam(value="文本邮件对象")@RequestBody SimpleTextMailDto mail){
 		mailSender.send(mail);
 	}
 }
